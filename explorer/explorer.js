95934ae0b98057fd49243749e7f45bbb585c0faa
Eric Boucher <eric.p.boucher@gmail.com>
2018-06-26 18:40:04 -0700

    Add languages logic

--- explorer/explorer.js
+++ explorer/explorer.js
@@ -5,6 +5,11 @@ var projects = JSON.parse(f);
 var html = ''
 for (var key in projects) {
     var project = projects[key];
+    var languages = ''
+    for (var index in project.languages) {
+        var language = project.languages[index]
+        languages += ('<div class="btn btn-light fixed-width nonprofit" style="margin: 10px;">' + language + '</div>')
+    }
     html += (
         '<div class="col-lg-4 col-sm-6">' +
         '<a class="portfolio-box" target="_blank" href="' + project.github + '">' +
@@ -13,6 +18,8 @@ for (var key in projects) {
         '<div class="portfolio-box-caption-content">' +
         '<div class="project-category text-faded">' + project.category + '</div>' +
         '<div class="project-name">' + project.description + '</div>' +
+        languages +
+        '<br>' +
         '<i class= "fa fa-github" ></i >' +
         '</div ></div ></a >' +
         '</div >'
